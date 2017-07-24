9475299d9e678d9cd3078405bc36114d16973056
dimaq12 <dimaq12@yandex.ru>
2017-07-24 18:28:01 +0300

    add data, inheritance, data service logic

--- src/classes/drone.js
+++ src/classes/drone.js
@@ -2,7 +2,9 @@ import {Vehicle} from './Vehicle.js'
 
 
 export class Drone extends Vehicle {
-	// constructor(){
-	// 	super()
-	// }
+	constructor(license, model, latLong){
+		super(license, model, latLong);
+		this.airTimeHours = null;
+		this.base = null;
+	}
 }
