462648588831d3ad4216f296cdfdf9689d8a56bb
dimaq12 <dimaq12@yandex.ru>
2017-07-28 23:33:04 +0300

    add data table class

--- src/app.js
+++ src/app.js
@@ -8,9 +8,20 @@ import {FleetDataService} from './services/fleet-data-service.js';
 import {TitleBar} from "./ui/title-bar.js";
 import {Button} from "./ui/button.js";
 import {Image} from "./ui/image.js";
+import {DataTable} from "./ui/data-table.js";
 
-let titleBar = new TitleBar('Click Me');
-titleBar.appendToElement($('body'));
+
+let headers = 'License Miles Model Miles'.split(' ');
+let dataService = new FleetDataService();
+dataService.loadData(fleet)
+let dt = new DataTable(headers, dataService.cars);
+dt.appendToElement($('body'));
+// let titleBar = new TitleBar('Click Me');
+// titleBar.addLink('Home', '')
+// titleBar.addLink('Cars', '')
+// titleBar.addLink('Drones', '')
+// titleBar.addLink('Map', '')
+// titleBar.appendToElement($('body'));
 
 
 // let im = new Image('src/images/drone.jpg');
